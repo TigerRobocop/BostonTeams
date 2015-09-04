88712f0bf7b8b70841d143807370acfda2c31d2c
LivSouza <liviarsouza@gmail.com>
2015-09-03 23:24:40 -0300

    implement ViewHolder pattern and convertView for recycling views

--- app/src/main/java/com/training/lsouza/bostonteams/TeamAdapter.java
+++ app/src/main/java/com/training/lsouza/bostonteams/TeamAdapter.java
@@ -1,12 +1,14 @@
 package com.training.lsouza.bostonteams;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import com.training.lsouza.bostonteams.R;
 import com.training.lsouza.bostonteams.Team;
 
@@ -14,8 +16,6 @@ import java.util.List;
 
 /**
  * Created by Livia on 01/09/2015.
- *
- *
  */
 public class TeamAdapter extends ArrayAdapter<Team> {
 
@@ -26,24 +26,38 @@ public class TeamAdapter extends ArrayAdapter<Team> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        //01 implement view
-        View view = LayoutInflater.from(getContext()).inflate(R.layout.team_item, null);
+        ViewHolder viewHolder;
+
+        //01 implement view - UPDATED
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.team_item, null);
+            viewHolder = new ViewHolder();
+            viewHolder.imgLogo = (ImageView) convertView.findViewById(R.id.img_logo);
+            viewHolder.txtSport = (TextView) convertView.findViewById(R.id.textView_sport);
+            viewHolder.txtName = (TextView) convertView.findViewById(R.id.textView_teamName);
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
 
         //02 get item at position
         Team team = getItem(position);
 
         //03 setup data into layout
-        ImageView imgLogo = (ImageView)view.findViewById(R.id.img_logo);
-        TextView txtSport = (TextView)view.findViewById(R.id.textView_sport);
-        TextView txtName = (TextView)view.findViewById(R.id.textView_teamName);
-
-        imgLogo.setImageResource(Util.getLogo(team.teamID));
-        txtSport.setText(team.sport);
-        txtName.setText(team.name);
+        if (team != null) {
+            viewHolder.imgLogo.setImageResource(Util.getLogo(team.teamID));
+            viewHolder.txtSport.setText(team.sport);
+            viewHolder.txtName.setText(team.name);
+        }
 
         //04 return view
-        return view;
+        return convertView;
     }
+}
 
-
+class ViewHolder {
+    ImageView imgLogo;
+    TextView txtSport;
+    TextView txtName;
 }
